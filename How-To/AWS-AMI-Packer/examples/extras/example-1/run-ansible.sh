dd0308ad2c1e4b95facd93dc7e6277a1e693456c
Matt Vander Werf <mvanderw@nd.edu>
2020-11-18 11:10:54 -0500

    Add all extras files for Packer example 1

---
+++ How-To/AWS-AMI-Packer/examples/extras/example-1/run-ansible.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -e
+
+usage="Ansible Script for Setting Up VM
+
+$(basename "$0") [-h]
+
+where:
+  -h   Show this help text
+  -f   Role/playbook name (default: base-os) [base-os]
+  -r   Run method (default: local) [local|packer]
+"
+
+if [ $# -le 1 ]; then
+    echo "$usage"
+    exit 1
+fi
+
+# default variable values
+ANSIBLE_DIR=/tmp/ansible
+PLAYBOOK_FILE=base-os.yml
+#TOP_DIR=/vault/install
+RUN_METHOD=local
+
+# parsing options
+while getopts ':h:f:i:c:t:s:r:' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    f) PLAYBOOK_FILE=$OPTARG.yml
+       ;;
+    i) TOP_DIR=$OPTARG
+       ;;
+    r) RUN_METHOD=$OPTARG
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+### If running through Packer
+if [ "$RUN_METHOD" == "packer" ]; then
+    cd $ANSIBLE_DIR
+fi
+
+if [ ! -f $PLAYBOOK_FILE ]; then
+    echo -e "Unsupported playbook. File $PLAYBOOK_FILE does not exist"
+    exit 1
+fi
+
+#### If running locally to test
+if [ "$RUN_METHOD" == "local" ]; then
+    mkdir -p $ANSIBLE_DIR/
+    cd $ANSIBLE_DIR/
+    tar xzf $TOP_DIR/ansible.tar.gz
+fi
+
+# install ansible
+if ! (which ansible-playbook) >/dev/null 2>&1; then
+    # prereqs
+    yum install -y python3 python3-pip
+
+    # install ansible
+    pip3 install ansible
+fi
+
+# exporting environment variables
+export ANSIBLE_INSTALL_SOURCE_DIR=$TOP_DIR
+
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin
+
+# running ansible
+ansible-playbook --verbose --connection=local -i inventory/hosts.yml ${PLAYBOOK_FILE}
+
+if [ "$RUN_METHOD" == "packer" ]; then
+  rm -rf $ANSIBLE_DIR
+fi
+
